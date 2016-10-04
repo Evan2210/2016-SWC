190b017a047f2f32b6e169af61e0eba1d45aaeaa
Aiwen Cheng <cheng.ai@husky.neu.edu>
2016-10-04 16:15:28 -0400

    111

--- ps-03/SOL LOWITT CANVAS.js
+++ ps-03/SOL LOWITT CANVAS.js
@@ -1,24 +1,33 @@
 // JavaScript Document var canvas = document.getElementById('canvas');
-    var ctx = c.getContext('2d');
+    var c=document.getElementById("canvas")
+    var ctx = c.getContext("2d");
 	var imgaa=document.getElementById("a");
 	var imgbb=document.getElementById("b");
 	var imgcc=document.getElementById("c");
 	var imgdd=document.getElementById("d");
+	
 	var pat1=ctx.createPattern(imgaa,"repeat");
 	var pat2=ctx.createPattern(imgbb,"repeat");
 	var pat3=ctx.createPattern(imgcc,"repeat");
 	var pat4=ctx.createPattern(imgdd,"repeat");
 	
     
-    ctx.fillRect(0, 0, 225, 100);
+    ctx.rect(0, 0, 225, 100);
 	ctx.fillStyle=pat1;
 	ctx.fill();
-	ctx.fillRect(225, 0, 450, 100);
+	ctx.rect(225, 0, 450, 100);
 	ctx.fillStyle=pat2;
 	ctx.fill();
-	ctx.fillRect(0,100, 225, 200);
+	ctx.rect(0,100, 225, 200);
 	ctx.fillStyle=pat3;
 	ctx.fill();
-	ctx.fillRect(225, 100, 450, 200); 
+	ctx.rect(225, 100, 450, 200); 
 	ctx.fillStyle=pat4;
 	ctx.fill();
+	/*var c=document.getElementById("myCanvas");
+	var ctx=c.getContext("2d");
+	var img=document.getElementById("lamp");
+	var pat=ctx.createPattern(img,"repeat");
+	ctx.rect(0,0,150,100);
+	ctx.fillStyle=pat;
+	ctx.fill();*/
