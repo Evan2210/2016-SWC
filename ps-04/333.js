a67e342ddbae2031ba73df99bee179fbd8a50b8d
Aiwen Cheng <cheng.ai@husky.neu.edu>
2016-10-11 17:34:14 -0400

    a

--- ps-04/333.js
+++ ps-04/333.js
@@ -4,7 +4,7 @@
         canvas=document.getElementById("canvas");
         ctx=canvas.getContext("2d");
         ctx.fillStyle="rgba(255,255,255,0.0)";
-        ctx.lineWidth=5;
+        ctx.lineWidth=2;
         ctx.lineJoin="miter";
         //int
        
@@ -15,11 +15,7 @@
             starx=Math.random()*canvas.width;
             stary=Math.random()*canvas.height;
 			ctx.lineTo(starx,stary);
-            var r=Math.round(Math.random()*255);
-            var g=Math.round(Math.random()*255);
-            var b=Math.round(Math.random()*255);
-            ctx.strokeStyle="rgba("+r+","+g+","+b+",1)";
-            ctx.stroke();
+                       ctx.stroke();
 			
 			}
 	  
