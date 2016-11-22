bd30cb710a21638739264dd811a7c1c615c79ae7
Aiwen Cheng <cheng.ai@husky.neu.edu>
2016-11-22 18:01:46 -0500

    bbb

--- ps-10/chart.js
+++ ps-10/chart.js
@@ -3,29 +3,26 @@ var s = Snap("#bar_chart");
 var button_1 = Snap("#button_1");
 var button_2 = Snap("#button_2");
 
-//create button
+
 button_1.rect(200,10, 90, 20, 2).attr({fill:"#FFF",stroke:"#000"});
 
-//create 2nd button by cloning the 1st
-//button_2 = button_1.clone();
 
-//add text to buttons
 var label_1 = button_1.text(220, 23,"Start");
-//var label_2 = button_2.text(50, 20,"Product 2");
 
-//create x and y axes
+
+
 var vertical = s.path("M45 10 V 450");
 var horizontal = s.path("M45 450 H 480");
 
-//put both axes into one group
+
 var axes = s.group(vertical, horizontal);
-//apply stroke to both axes at once via the group
+
 axes.attr({
         stroke: "#000",
         strokeWidth: 5
     });
 
-//add increment labels
+
 var vertical_1 = s.text(10, 450,"0");
 var vertical_2 = s.text(5, 360,"15");
 var vertical_3 = s.text(5, 270,"30");
@@ -41,7 +38,7 @@ var horizontal_5 = s.text(280, 470,"Friday");
 var horizontal_6 = s.text(340, 485,"Saturday");
 var horizontal_7 = s.text(400, 470,"Sunday");
 
-//create bars
+
 var bar1 = s.rect(50, 440, 40, 10, 10).attr({
     fill: "#bada55"
 });
@@ -70,15 +67,15 @@ var bar7 = s.rect(410, 440, 40, 10, 10).attr({
 
 
 
-//put the 5 bars into one group
+
 var bars = s.group(bar1, bar2, bar3, bar4, bar5,bar6,bar7);
-//apply stroke to all bars at once via the group
+
 bars.attr({
     stroke: "#000",
     strokeWidth: 2
 });
 
-//add click event listeners
+
 button_1.click(function () {
   bar1.animate({height:180, y:270}, 2000);
   bar2.animate({height:40, y:410}, 2000);
